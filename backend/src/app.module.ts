8b9894e5d58e5d14745a5464e45aa14908d6e6bd
Paul <p-kondratov@mail.ru>
2022-04-13 04:13:44 +0500

    git event controller included

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GitEventController } from './git-events/git-event.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, GitEventController],
   providers: [AppService],
 })
 export class AppModule {}
